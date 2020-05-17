5ee65d168a5956adaaf388c89c5a3f50a6233574
sizhky <1992chinna@gmail.com>
2020-05-17 17:27:55 +0530

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 from torch import optim
 from torch.nn import functional as F
 from torch.utils.data import Dataset, DataLoader
+import path
 import xml.etree.ElementTree as ET
 from torchvision import transforms
 
